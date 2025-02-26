5b326209383822b82da2c2fae4351c37ff72c90d
Tâmara Thais <t.thais@escolar.ifrn.edu.br>
2025-02-26 15:51:04 -0300

    Update ArquivoRepository.java

--- src/repository/ArquivoRepository.java
+++ src/repository/ArquivoRepository.java
@@ -4,30 +4,35 @@ import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ArquivoRepository<T> {
-    private String nomeArquivo;
+public class ArquivoRepository {
+    private static final String DIRETORIO = "dados/";
 
-    public ArquivoRepository(String nomeArquivo) {
-        this.nomeArquivo = nomeArquivo;
-    }
+    public void salvar(Object objeto) {
+        String nomeArquivo = DIRETORIO + objeto.getClass().getSimpleName() + ".dat";
+        List<Object> objetos = carregarTodos(objeto.getClass());
+        objetos.add(objeto);
 
-    public void salvar(List<T> items) {
-        try (ObjectOutputStream oos = new ObjectOutputStream(
-                new FileOutputStream(nomeArquivo))) {
-            oos.writeObject(new ArrayList<>(items));
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(nomeArquivo))) {
+            oos.writeObject(objetos);
         } catch (IOException e) {
-            System.err.println("Erro ao salvar arquivo: " + e.getMessage());
+            e.printStackTrace();
         }
     }
 
-    @SuppressWarnings("unchecked")
-    public List<T> carregar() {
-        try (ObjectInputStream ois = new ObjectInputStream(
-                new FileInputStream(nomeArquivo))) {
-            return (List<T>) ois.readObject();
+    public <T> List<T> carregarTodos(Class<T> classe) {
+        String nomeArquivo = DIRETORIO + classe.getSimpleName() + ".dat";
+        List<T> objetos = new ArrayList<>();
+
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(nomeArquivo))) {
+            objetos = (List<T>) ois.readObject();
+        } catch (FileNotFoundException e) {
+            // Arquivo ainda não existe, retorna lista vazia
         } catch (IOException | ClassNotFoundException e) {
-            System.err.println("Erro ao carregar arquivo: " + e.getMessage());
-            return new ArrayList<>();
+            e.printStackTrace();
         }
+
+        return objetos;
     }
+
+    // Outros métodos conforme necessário
 }
